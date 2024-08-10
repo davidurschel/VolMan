2de3ec88b78142e0fbed901bf4b92021289531df
davidurschel <durschel@ualberta.ca>
2024-08-09 23:40:35 -0700

    Moved config to be in APPDATA by default

--- pkg/src/config_functions.py
+++ pkg/src/config_functions.py
@@ -1,22 +1,27 @@
 import json
 import os
+import constants
 
-def save_config(com_port, baud_rate, rails, config_path='config.cfg'):
+def get_config_path(CONFIG_FILENAME):
+    app_data_path = os.getenv('APPDATA')
+    return os.path.join(app_data_path, 'VolMan', CONFIG_FILENAME)
+
+def save_config(com_port, baud_rate, rails, config_filename=constants.CONFIG_FILENAME):
     config_dict = {}
     config_dict['COM_PORT'] = com_port
     config_dict['BAUD_RATE'] = baud_rate
     config_dict['RAILS'] = rails
-    with open(config_path, 'w') as f:
+    with open(get_config_path(config_filename), 'w') as f:
         json.dump(config_dict, f)
         f.close()
 
 '''Returns: com_port:str, baud_rate:str, rails:dict'''
-def load_config(config_path='config.cfg') -> tuple:
-    if not os.path.isfile(config_path):
+def load_config(config_filename=constants.CONFIG_FILENAME) -> tuple:
+    if not os.path.isfile(get_config_path(config_filename)):
         save_config("", "", {})
     config_dict = {'COM_PORT': '', 'BAUD_RATE': '', 'RAILS':''}
     try:
-        with open(config_path, 'r') as f:
+        with open(get_config_path(config_filename), 'r') as f:
             config_dict = json.load(f)
             f.close()
     except: 
--- pkg/src/constants.py
+++ pkg/src/constants.py
@@ -1,4 +1,5 @@
 ICON_FILE = 'icon.ico'
 HELP_URL = 'https://github.com/davidurschel/VolMan'
 CONFIG_HELP_URL = 'https://github.com/davidurschel/VolMan'
+CONFIG_FILENAME = "config.cfg"
 BAUD_RATES = ["300", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200"]
