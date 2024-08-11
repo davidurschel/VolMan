d8cb64d73b30140c0328dde8f66e4388eb73d229
David Urschel <92534511+davidurschel@users.noreply.github.com>
2024-08-10 17:19:00 -0700

    Update main.ino

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -1,5 +1,9 @@
 // Fill with the numbers for all the pins that read a potentiometer
-int potPins[] = {A0, A1, A2, A3};
+int potPins[] = {A0, A1, A2, A3, A4};
+
+// Choose the BAUD rate for the serial connection
+int baudRate = 9600
+
 int potCnt;
 
 // Fill with numbers for all the pins that read a button
@@ -7,13 +11,12 @@ int butPins[] = {};
 int butCnt;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(baudRate);
   potCnt = sizeof(potPins)/sizeof(potPins[0]);
 }
 
 void loop() {
   if (sizeof(potPins) > 0){
-    
     for(int i = 0; i < potCnt; i++){
       float sensorValue = (float)analogRead(potPins[i]);
       float out = sensorValue/1023.0;
